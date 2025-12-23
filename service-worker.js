a53ac42334b618c35bd42e2c4acafd14bf64a7cb
tesbihatapp <tesibhatapp@gmail.com>
2025-12-23 15:45:36 -0500

    fix v2

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
    =================================================================== */
 
 /* Versiyon numarasını her “önemli” değişimde artır ki eski cache temizlensin. */
-const CACHE_VERSION = 'v60';
+const CACHE_VERSION = 'v61';
 
 /* Scope → /tesbihat/  veya  /tesbihat/staging/  tespiti */
 const SCOPE_URL = self.registration && self.registration.scope ? new URL(self.registration.scope) : new URL('/', self.location.origin);
